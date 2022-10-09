03ef6429750e13c09b72be5e9af1e1f906789c83
reydinerr <reydiner@protonmail.com>
2022-10-09 13:09:21 -0300

    feat: ensure signupcontroller return an error 400

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,6 +2,7 @@ export class SignUpController {
     handler(httpRequest: any): any {
         return {
             statusCode: 400,
+            body: new Error("Missing param: name"),
         }
     }
 }
