b025be643b5461b4f06183fc6cf9ed5a71bcc781
reydinerr <reydiner@protonmail.com>
2022-10-13 22:33:48 -0300

    refactor: move sucess http response to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,3 +1,4 @@
+import { AnyARecord } from "dns"
 import { ServerError } from "../error/server-error"
 import { HttpResponse } from "../protocols/http"
 
@@ -5,7 +6,13 @@ export const badRequest = (error: Error): HttpResponse => ({
     statusCode: 400,
     body: error,
 })
+
 export const serverError = (): HttpResponse => ({
     statusCode: 500,
     body: new ServerError(),
+})
+
+export const ok = (data: any): HttpResponse => ({
+    statusCode: 200,
+    body: data,
 })
